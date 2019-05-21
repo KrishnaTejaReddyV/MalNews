145ef0981fbcfa796c7bb18ed5dbd2a30677c904
Teja <vkrishr.teja@gmail.com>
2019-05-21 03:46:08 -0400

    Added manifest icons

--- frontend/public/customServiceWorker.js
+++ frontend/public/customServiceWorker.js
@@ -1,32 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-    "/precache-manifest.cec4a4b2c9c5ee2bced799ec5d420df3.js"
-);
-  
-self.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-      self.skipWaiting();
-    }
-});
-  
-workbox.core.clientsClaim();
-  
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-  
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-    
-    blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
-});
   
 workbox.routing.registerRoute(
-    /.*\/graphql/,
+    /.*\/(?:graphql|malware|vulnerabilities|cyber_threat|cloud_security|software_exploits|search)/,
     workbox.strategies.networkFirst()
 )
 

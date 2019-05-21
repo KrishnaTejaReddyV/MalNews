1211645411e2673c336d08772999d5ccd397798b
Teja <vkrishr.teja@gmail.com>
2019-05-21 04:19:03 -0400

    fixed icon, https, and landing url error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import HttpsRedirect from 'react-https-redirect';
 
 import Reducer from './store/Reducer';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
 const store = createStore(Reducer);
 
-ReactDOM.render(<Provider store={store}><HttpsRedirect><App /></HttpsRedirect></Provider>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));

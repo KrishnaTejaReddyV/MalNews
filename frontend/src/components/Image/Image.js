52a46d9ddd685eee70d821fd843d7d09dfbe2764
Teja <vkrishr.teja@gmail.com>
2019-05-21 05:07:47 -0400

    Replaced http image sources with https

--- frontend/src/components/Image/Image.js
+++ frontend/src/components/Image/Image.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const Image = props => (
-    <img 
-        src={props.src} 
-        alt="Not found" 
-        onError={
-            (e) => {
-                e.target.onerror = null;
-                e.target.src='https://www.cisco.com/c/en/us/products/security/advanced-malware-protection/what-is-malware/_jcr_content/Grid/category_atl_9819/layout-category-atl/anchor_info_a0df/image.img.png/1526550535037.png';
-            }
-        } 
-    />
-)
+const Image = props => {
+    const imgSrc = (props.src || "").replace(/^http:\/\//i, 'https://');
+
+    return (
+        <img 
+            src={imgSrc} 
+            alt="Not found" 
+            onError={
+                (e) => {
+                    e.target.onerror = null;
+                    e.target.src='https://www.cisco.com/c/en/us/products/security/advanced-malware-protection/what-is-malware/_jcr_content/Grid/category_atl_9819/layout-category-atl/anchor_info_a0df/image.img.png/1526550535037.png';
+                }
+            } 
+        />
+    );
+}
 
 export default Image;
